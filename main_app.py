26bbbaad321225f22288d72a3bc991d30990eb63
amitchew <49229715+amitchew@users.noreply.github.com>
2022-12-05 11:59:55 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -15,12 +15,6 @@ with pdfplumber.open(uploaded_files) as pdf:
     page = pdf.pages[0]
     final_text = page.extract_text()
      
-
-for uploaded_file in uploaded_files:
-     bytes_data = uploaded_file.read()
-     image = Image.open(io.BytesIO(bytes_data))
-     st.write("filename:", uploaded_file.name)
-     st.image(image)
  
 
 st.text_area(label="Output Data:", value=final_text, height=550)
