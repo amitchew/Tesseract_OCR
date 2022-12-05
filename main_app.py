ae72de4932109849ea68315fb82e768b96d41de8
amitchew <49229715+amitchew@users.noreply.github.com>
2022-12-05 10:54:23 +0300

    Create main_app.py

---
+++ main_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import io
+from PIL import Image
+
+
+uploaded_files = st.file_uploader("Veuillez charger une image",type=['jpg','jpeg','png'],help="Charger une image au format jpg,jpeg,png", accept_multiple_files=True,)
+
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     image = Image.open(io.BytesIO(bytes_data))
+     st.write("filename:", uploaded_file.name)
+     st.image(image)
