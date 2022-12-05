7dbb985022399adf975c86dc1494985cbfa6bd52
amitchew <49229715+amitchew@users.noreply.github.com>
2022-12-05 11:55:21 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -23,6 +23,4 @@ for uploaded_file in uploaded_files:
      st.image(image)
  
 
-output = input.upper() # final_result_from_processing_the_input
-
 st.text_area(label="Output Data:", value=final_text, height=550)
