6b4f73443d6869c102d2fee2166138c1181c9b9b
amitchew <49229715+amitchew@users.noreply.github.com>
2022-12-14 14:59:53 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -16,13 +16,13 @@ uploaded_files = st.file_uploader("Upload file",type=['pdf'],help="Upload files 
 templates= read_templates('Template/')
 result_final= extract_data(uploaded_files, templates=templates)
 
-# with pdfplumber.open(uploaded_files) as pdf:
-#     page = pdf.pages[0]
-#     final_text = page.extract_text()
+with pdfplumber.open(uploaded_files) as pdf:
+    page = pdf.pages[0]
+    final_text = page.extract_text()
      
  
 
-# st.text_area(label="Output Data:", value=final_text, height=550)
+st.text_area(label="Output Data:", value=final_text, height=550)
 
 st.text_area(label="Extracted Data:", value=result_final, height=250)
 
