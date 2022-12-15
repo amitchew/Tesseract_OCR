06a60deb2750a51ced52723492b8ba5d52891ede
amitchew <49229715+amitchew@users.noreply.github.com>
2022-12-15 13:24:18 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -15,8 +15,8 @@ st.header('OCR for Sparta X')
 uploaded_files = st.file_uploader("Upload file",type=['pdf'],help="Upload files in pdf", accept_multiple_files=False,)
 
 
-# templates= read_templates('Template/')
-# result_final= extract_data('AmazonWebServices.pdf', templates=templates)
+templates= read_templates('Template/')
+result_final= extract_data('AmazonWebServices.pdf', templates=templates)
 
 
 with pdfplumber.open(uploaded_files) as pdf:
@@ -25,7 +25,7 @@ with pdfplumber.open(uploaded_files) as pdf:
      
  
 
-st.text_area(label="Output Data:", value=final_text, height=550)
+# st.text_area(label="Output Data:", value=final_text, height=550)
 
-# st.text_area(label="Extracted Data:", value=result_final, height=250)
+st.text_area(label="Extracted Data:", value=result_final, height=250)
 
